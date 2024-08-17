2f86778930ba34883ff2aedd4102320a0174c899
Jakal02 <gitcontribs@eligrubbs.com>
2024-08-17 11:15:13 -0500

    test for predict route.

--- backend/tests/api/test_model_routes.py
+++ backend/tests/api/test_model_routes.py
@@ -1,6 +1,8 @@
 import pytest
 from httpx import AsyncClient, Response
 
+from app.schemas.observation_schema import ObservationModel
+
 
 @pytest.mark.anyio
 async def test_is_fit_route(client: AsyncClient):
@@ -8,3 +10,16 @@ async def test_is_fit_route(client: AsyncClient):
     response: Response = await client.get("/model/is_fitted")
 
     assert response.json() == {"model status": True}
+
+@pytest.mark.anyio
+async def test_predict_route(client: AsyncClient):
+    """Test that model predicts"""
+    data = ObservationModel(sepal_length=5,
+                            sepal_width=5,
+                            petal_length=5,
+                            petal_width=3)
+
+    response: Response = await client.post("/model/predict",
+                                           json=data.model_dump())
+
+    assert response.json()['species'] in ["sertosa", "virginica", "versicolor"]
